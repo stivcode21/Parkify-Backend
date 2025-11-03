684027e93c5e915758d1b22c707504a12f8e0106
stivcode21 <stivenelcodificador@gmail.com>
2025-11-02 19:54:54 -0500

    feat: created authRouter to control endpoints

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const auth = require("../controllers/authController.js");
+const { verifyToken } = require("../middleware/authMiddleware.js");
+
+const router = express.Router();
+
+router.post("/login", auth.loginController);
+// router.post("/logout", auth.logoutController);
+router.get("/dashboard", verifyToken, (req, res) => {
+  res.json({ message: "Acceso concedido", user: req.user });
+});
+
+module.exports = router;
+
