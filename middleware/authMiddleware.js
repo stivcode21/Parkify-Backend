e4e7aad729f2a7ccad43b3c816c247f401602778
stivcode21 <stivenelcodificador@gmail.com>
2025-11-02 19:56:04 -0500

    feat: created authMiddleware for verifyToken

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ message: "Acceso denegado. No hay token." });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Token inválido o expirado." });
+  }
+};
