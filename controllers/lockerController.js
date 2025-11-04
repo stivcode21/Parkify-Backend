fb39813d3b5324a440a97c9eaf21cb85654dbfdd
stivcode21 <stivenelcodificador@gmail.com>
2025-11-03 22:50:51 -0500

    I created a fully functional Controller for Lockers table

---
+++ controllers/lockerController.js
@@ -0,0 +1,19 @@
+const { getLockersByAdminId } = require("../models/lockerModel");
+
+exports.getLockersController = async (req, res) => {
+  try {
+    const adminId = req.user.id_admin; //viene del token
+
+    if (!adminId) {
+      return res
+        .status(401)
+        .json({ message: "No se pudo identificar el administrador." });
+    }
+
+    const lockers = await getLockersByAdminId(adminId);
+    return res.status(200).json({ lockers });
+  } catch (error) {
+    console.error("Error al obtener casilleros:", error);
+    return res.status(500).json({ message: "Error interno del servidor." });
+  }
+};
