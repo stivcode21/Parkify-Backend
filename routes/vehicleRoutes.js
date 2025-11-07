4f2662153b55f64b64ad6efc10aec47443518b98
stivcode21 <stivenelcodificador@gmail.com>
2025-11-07 17:56:49 -0500

    feat: create endpoints for new controllers in vehicles

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -4,9 +4,13 @@ const { verifyToken } = require("../middleware/authMiddleware.js");
 const {
   entryVehicleController,
   listVehiclesController,
+  exitVehicleController,
+  searchVehicleController,
 } = require("../controllers/vehicleController.js");
 
 router.post("/entry", verifyToken, entryVehicleController);
 router.get("/list", verifyToken, listVehiclesController);
+router.post("/exit", verifyToken, exitVehicleController);
+router.post("/search", verifyToken, searchVehicleController);
 
 module.exports = router;
