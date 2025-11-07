01ba66a4efcf0d0eeede0b68e798395336f63f7b
stivcode21 <stivenelcodificador@gmail.com>
2025-11-06 21:13:40 -0500

    feat: create new endpoint to listVehiclesController

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { verifyToken } = require("../middleware/authMiddleware.js");
 const {
   entryVehicleController,
+  listVehiclesController,
 } = require("../controllers/vehicleController.js");
 
 router.post("/entry", verifyToken, entryVehicleController);
+router.get("/list", verifyToken, listVehiclesController);
 
 module.exports = router;
