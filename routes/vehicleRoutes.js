7596d1d371951e4fc18f97eb2111c6f60005d8d6
stivcode21 <stivenelcodificador@gmail.com>
2025-11-07 18:33:01 -0500

    feat: defined mew endpoint 'records'

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -6,11 +6,13 @@ const {
   listVehiclesController,
   exitVehicleController,
   searchVehicleController,
+  getRecordsController,
 } = require("../controllers/vehicleController.js");
 
 router.post("/entry", verifyToken, entryVehicleController);
 router.get("/list", verifyToken, listVehiclesController);
 router.post("/exit", verifyToken, exitVehicleController);
 router.post("/search", verifyToken, searchVehicleController);
+router.get("/records", verifyToken, getRecordsController);
 
 module.exports = router;
