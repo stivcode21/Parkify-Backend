198b20a5497e1045ebbe38f8d4d660f7e715460a
stivcode21 <stivenelcodificador@gmail.com>
2026-01-19 23:29:38 -0500

    update: small change in cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,9 @@
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://tu-frontend.com"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://tu-frontend.com",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
