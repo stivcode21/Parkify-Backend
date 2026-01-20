d32fa17559f697dd97f66035b2a7299fe16a3012
stivcode21 <stivenelcodificador@gmail.com>
2026-01-19 23:34:57 -0500

    feat: All models were updated for compatibility with PostgreSQL.

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,6 +1,9 @@
 const db = require("../config/db");
 
 exports.findAdminByEmail = async (email) => {
-  const [rows] = await db.query("SELECT * FROM admins WHERE correo = ?", [email]);
+  const { rows } = await db.query(
+    "SELECT * FROM admins WHERE correo = $1",
+    [email]
+  );
   return rows.length > 0 ? rows[0] : null;
 };
--- models/lockerModel.js
+++ models/lockerModel.js
@@ -1,8 +1,8 @@
 const db = require("../config/db");
 
 exports.getLockersByAdminId = async (adminId) => {
-  const [rows] = await db.query(
-    "SELECT * FROM casilleros WHERE id_admin = ? ORDER BY id_casillero ASC",
+  const { rows } = await db.query(
+    "SELECT * FROM casilleros WHERE id_admin = $1 ORDER BY id_casillero ASC",
     [adminId]
   );
   return rows;
