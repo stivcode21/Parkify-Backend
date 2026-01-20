dbb0b324b23fa44f66017ee5fe583f6ff4892c46
stivcode21 <stivenelcodificador@gmail.com>
2026-01-19 23:30:10 -0500

    feat: change conexion with db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
-const mysql = require("mysql2/promise");
+const { Pool } = require("pg");
 
-const db = mysql.createPool({
+const db = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = db;
